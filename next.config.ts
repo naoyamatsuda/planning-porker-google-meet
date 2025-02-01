930a66531235d3985b94e0bf659aeea9d0aaad80
松田尚也 <matsuda-naoya@MacBook-Pro.local>
2025-02-01 14:53:03 +0900

    Deploy Next.js static export to gh-pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/planning-porker-google-meet",
 };
 
 export default nextConfig;
